969c177ce075fb4bf6ceca7bb43f0c1993fa1288
zhangjiechina001 <295737015@qq.com>
2019-05-13 23:41:39 +0800

    宿舍电脑

--- LionsDetectHog+SVM/main.py
+++ LionsDetectHog+SVM/main.py
@@ -29,5 +29,59 @@ hog=cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nBin)
 svm=cv2.ml.SVM_create()
 
 featureNum=int(((128-16)/8+1)*((64-16)/8+1)*4*9)#3780
-fetureArray=np.zeros((PosNum+NegNum),featureNum,dtype=np.float32)
-labelArray=np.zeros(((PosNum+NegNum),featureNum),dtype=np.float32)
+fetureArray=np.zeros(((PosNum+NegNum),featureNum),dtype=np.float32)
+labelArray=np.zeros((((PosNum+NegNum),featureNum)),dtype=np.float32)
+
+for i in range(0,PosNum):
+    fileNmae='pos\\'+str(i+1)+'.jpg'
+    img=cv2.imread(fileNmae)
+    img=cv2.cvtColor(img,cv2.CV_32SC1)
+    his=hog.compute(img,(8,8))
+    for j in range(0,featureNum):
+        fetureArray[i,j]=his[j]
+    labelArray[i,0]=1
+        #z正样本label 1
+
+for i in range(0,NegNum):
+    fileNmae='neg\\'+str(i+1)+'.jpg'
+    img = cv2.imread(fileNmae)
+    img=cv2.cvtColor(img,cv2.CV_32SC1)
+    his = hog.compute(img, (8, 8))
+    for j in range(0, featureNum):
+        fetureArray[i+PosNum,j] = his[j]
+    labelArray[i+PosNum, 0] = -1
+
+svm.setType(cv2.ml.SVM_C_SVC)
+svm.setKernel(cv2.ml.SVM_LINEAR)
+svm.setC(0.01)
+
+# train
+ret=svm.train(fetureArray,cv2.ml.ROW_SAMPLE,labelArray)
+#7检测
+alpha=np.zeros((1),np.float32)
+rho=svm.getDecisionFunction(0,alpha)
+# rho=svm.getDecisionFunction(0,)
+print(rho)
+print(alpha)
+alphaArray=np.zeros((1,1),np.float32)
+supportVArray=np.zeros({1,featureNum},np.float32)
+resultArray=np.zeros((1,featureNum),np.float32)
+alphaArray[0,0]=alpha
+
+mydetect=np.zeros((3781),np.float32)
+for i in range(0,3781-1):
+    mydetect[i]=resultArray[0,1]
+mydetect[3780]=rho[0]
+#构建hog
+myHog=cv2.HOGDescriptor()
+myHog.setSVMDetector(mydetect)
+
+imgSrc=cv2.imread('Test2.jpg')
+objs=myHog.detectMultiScale(imgSrc,0,(8,8),(32,32),1.05,2)
+x=int(objs[0][0][0])
+y=int(objs[0][0][1])
+w=int(objs[0][0][2])
+h=int(objs[0][0][3])
+cv2.rectangle(imgSrc,(x,y),(x+w,y+h),(255,0,0),2)
+cv2.imshow('result',imgSrc)
+cv2.waitKey()
--- pyQt5/ocrNum.py
+++ pyQt5/ocrNum.py
@@ -7,6 +7,37 @@ def imgThreshold(img):
     rosource,binary=cv2.threshold(img,121,255,cv2.THRESH_BINARY)
     return binary
 
+#1.先水平分割，再垂直分割
+# 对图片进行垂直分割
+def verticalCut(img,img_num):
+    (x,y)=img.shape #返回的分别是矩阵的行数和列数，x是行数，y是列数
+    pointCount=np.zeros(y,dtype=np.uint8)#每列黑色的个数
+    x_axes=np.arange(0,y)
+    #i是列数，j是行数
+    for i in range(0,y):
+        for j in range(0,x):
+            if(img[j,i]==0):
+                pointCount[i]=pointCount[i]+1
+    plt.plot(x_axes,pointCount)
+
+    start = []
+    end = []
+    # 对照片进行分割
+    print(pointCount)
+    for index in range(1, y):
+        # 上个为0当前不为0，即为开始
+        if ((pointCount[index] != 0) & (pointCount[index - 1] == 0)):
+            start.append(index)
+        # 上个不为0当前为0，即为结束
+        elif ((pointCount[index] == 0) & (pointCount[index - 1] != 0)):
+            end.append(index)
+
+    for idx in range(1,len(start)):
+        tempimg=img[ :,start[idx]:end[idx]]
+        cv2.imshow(str(img_num)+"_"+str(idx), tempimg)
+    # cv2.waitKey()
+    # plt.show()
+
 #对图片进行水平分割
 def horizontalCut(img):
     (x,y)=img.shape #返回的分别是矩阵的行数和列数，x是行数，y是列数
@@ -17,7 +48,6 @@ def horizontalCut(img):
             if(img[i,j]==0):
                 pointCount[i]=pointCount[i]+1
     plt.plot(x_axes,pointCount)
-    plt.show()
     start=[]
     end=[]
     #对照片进行分割
@@ -33,15 +63,25 @@ def horizontalCut(img):
     img2=img[start[1]:end[1],:]
     img3=img[start[2]:end[2],:]
     imgArr=[img1,img2,img3]
+    verticalCut(img1,1)
+    verticalCut(img2,2)
+    verticalCut(img3,3)
     for m in range(3):
         cv2.imshow(str(m),imgArr[m])
     cv2.waitKey()
+    plt.show()
+    return imgArr
+
+def matchTemplate(src,matchSrc):
+    srcimg,label=src
+    result=cv2.matchTemplate(matchSrc,src,cv2.TM_CCOEFF)
 
 img = cv2.imread("ocrdetect.jpg")
 img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 binary=imgThreshold(img)
 cv2.imshow('result',binary)
-horizontalCut(binary)#212*200
+returnImg=horizontalCut(binary)#212*200
+matchTemplate()
 
 
 
