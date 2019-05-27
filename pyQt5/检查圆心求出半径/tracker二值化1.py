5ae6c0202f9a34fe5ed2259b52444a5d6e4cdcc5
zhangjiechina001 <295737015@qq.com>
2019-05-27 17:29:05 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	pyQt5/检查圆心求出半径/unFloodAfterBinary.jpg

--- pyQt5/检查圆心求出半径/tracker二值化1.py
+++ pyQt5/检查圆心求出半径/tracker二值化1.py
@@ -3,7 +3,7 @@ import cv2
 import cv2
 import numpy as np
 
-img = cv2.imread("aftercutimg.jpg")
+img = cv2.imread("imgSave.jpg")
 img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow('img',img)
 cv2.namedWindow('binary',cv2.WINDOW_NORMAL)
